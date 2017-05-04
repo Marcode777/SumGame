62d3e6b403757d56d0e9a09475aa3d568d66fcb7
Marcode777 <marcosebello7@gmail.com>
2017-05-04 13:51:27 -0400

    added dynamic behavior to child

--- sumgame/src/quizoptions.js
+++ sumgame/src/quizoptions.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class QuizOptions extends React.Component{
   render(){
     return(
-        <div className="fields"><div className="field-block">10</div></div>      
+        <div className="fields">
+          <div className="field-block">{this.props.option}</div>
+        </div>      
       );
   }
 }
